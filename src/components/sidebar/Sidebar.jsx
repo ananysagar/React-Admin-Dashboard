21283fa670b3084ba6b09eea3e6334bfc6f51f2d
Anany Sagar <sagaranany577@gmail.com>
2024-04-06 13:11:57 +0530

    initial commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,22 @@
+import './sidebar.scss'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="top">
+        <span className='logo'>MyAdmin</span>
+      </div>
+      <div className="center">
+        <ul>
+            <li><span>Dashboard</span></li>
+            <li><span>Dashboard</span></li>
+            <li><span>Dashboard</span></li>
+            <li><span>Dashboard</span></li>
+        </ul>
+      </div>
+      <div className="bottom">color</div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "../../components/sidebar/Sidebar"
+import "./home.scss"
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Sidebar />
+      <div className="homeContainer">homeContainer</div>
+    </div>
+  )
+}
+
+export default Home
